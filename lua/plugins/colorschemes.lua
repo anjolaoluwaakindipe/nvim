51796a72e699a0d5501fdd3523310632f80aebcb
Anjolaoluwa Akindipe <anjaki36@ies-anjaki36-rk8.ies.mentorg.com>
2024-06-21 15:06:08 -0600

    added scroll bar

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,56 +3,63 @@ return {
 	-- tokyonight
 	{
 		"folke/tokyonight.nvim",
-	  priority = 1000,
 		opts = { style = "moon" },
-    config = function ()
-      local transparent = false -- set to true if you would like to enable transparency
-
-    local bg = "#011628"
-    local bg_dark = "#011423"
-    local bg_highlight = "#143652"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
-
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
-      end,
-    })
-
-    vim.cmd("colorscheme tokyonight")
-    end
+	  -- priority = 1000,
+    -- config = function ()
+    --   local transparent = true -- set to true if you would like to enable transparency
+    --   local bg = "#011628"
+    --   local bg_dark = "#011423"
+    --   local bg_highlight = "#143652"
+    --   local bg_search = "#0A64AC"
+    --   local bg_visual = "#275378"
+    --   local fg = "#CBE0F0"
+    --   local fg_dark = "#B4D0E9"
+    --   local fg_gutter = "#627E97"
+    --   local border = "#547998"
+    --
+    --   require("tokyonight").setup({
+    --     style = "night",
+    --     transparent = transparent,
+    --     styles = {
+    --       sidebars = transparent and "transparent" or "dark",
+    --       floats = transparent and "transparent" or "dark",
+    --     },
+    --     on_colors = function(colors)
+    --       colors.bg = bg
+    --       colors.bg_dark = transparent and colors.none or bg_dark
+    --       colors.bg_float = transparent and colors.none or bg_dark
+    --       colors.bg_highlight = bg_highlight
+    --       colors.bg_popup = bg_dark
+    --       colors.bg_search = bg_search
+    --       colors.bg_sidebar = transparent and colors.none or bg_dark
+    --       colors.bg_statusline = transparent and colors.none or bg_dark
+    --       colors.bg_visual = bg_visual
+    --       colors.border = border
+    --       colors.fg = fg
+    --       colors.fg_dark = fg_dark
+    --       colors.fg_float = fg
+    --       colors.fg_gutter = fg_gutter
+    --       colors.fg_sidebar = fg_dark
+    --     end,
+    --   })
+    --
+    --   vim.cmd("colorscheme tokyonight")
+    -- end
 	},
 
 	-- catppuccin
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+	  priority = 1000,
 		opts = {
+      term_colors = true,
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15
+      },
 			integrations = {
 				aerial = true,
 				alpha = true,
@@ -88,6 +95,11 @@ return {
 				treesitter_context = true,
 				which_key = true,
 			},
+      flavour = "mocha"
 		},
+    config = function (_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd("colorscheme catppuccin")
+    end
 	},
 }
